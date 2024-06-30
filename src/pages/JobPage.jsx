fbf64fa35c2e53f6efcce33b51a7f238c4151b72
Abdullah Al-Wadeiah <abdullah.aw14@gmail.com>
2024-06-30 08:43:36 -0600

    Implemented delete functionality and added toast messages

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,13 +1,24 @@
-import { useParams, useLoaderData } from "react-router-dom"
+import { useParams, useLoaderData, Link, useNavigate } from "react-router-dom"
 import { FaArrowLeft, FaMapMarker } from "react-icons/fa"
-import { Link } from "react-router-dom"
+import { toast } from "react-toastify"
 
-const JobPage = () => {
+const JobPage = ({deleteJob}) => {
+  const navigate = useNavigate()
     const { id } = useParams()      // getting the ID from the URL using React Hook
     const job = useLoaderData()
+
+    const onDeleteClick = (jobId) => {
+      const confirm = window.confirm('Are you sure you want to delete this job listing?')
+
+      if(!confirm) return
+
+      deleteJob(jobId)
+      toast.success('Job deleted successfully')
+      navigate('/jobs')
+    }
   return (
         <>
-          <section>
+    <section>
       <div className="container m-auto py-6 px-6">
         <Link
           to="/jobs"
@@ -85,6 +96,7 @@ const JobPage = () => {
                 >Edit Job</Link
               >
               <button
+                onClick={ () => onDeleteClick(job.id)}
                 className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
               >
                 Delete Job
